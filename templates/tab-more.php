621b4f4631be55214235f1b7767b32d73a2fa988
Frances Botsford <frances@switchingprotocols.com>
2017-05-25 11:27:03 -0400

    add header for website search

--- templates/tab-more.php
+++ templates/tab-more.php
@@ -38,7 +38,7 @@
 <form class="form search-site" action="https://www.google.com/cse" method="get" data-target="google">
 	<input type="hidden" name="cx" value="016240528703941589557:i7wrbu9cdxu">
 	<input type="hidden" name="ie" value="UTF-8">
-	<label for="searchinput-site">Search the library website</label>
+	<h3><label for="searchinput-site">Search the library website</label></h3>
 	<div class="wrap-flex">
 		<div class="flex-left">
 			<input id="searchinput-site" class="field field-text" type="text" name="q" placeholder="Search our site, location, guides, and support">
