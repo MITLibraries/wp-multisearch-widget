613b1f46850617ed0c8b66424f9f171a50362126
GitHub Action <action@github.com>
2022-03-02 22:27:27 +0000

    Updates plugin including built assets

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-1e40a584e89db943dafe76f80cf7e95dd8631c82',
+        'version' => 'dev-1e40a584e89db943dafe76f80cf7e95dd8631c82',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '1e40a584e89db943dafe76f80cf7e95dd8631c82',
+        'name' => 'mitlibraries/wp-multisearch-widget',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'mitlibraries/wp-multisearch-widget' => array(
+            'pretty_version' => 'dev-1e40a584e89db943dafe76f80cf7e95dd8631c82',
+            'version' => 'dev-1e40a584e89db943dafe76f80cf7e95dd8631c82',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '1e40a584e89db943dafe76f80cf7e95dd8631c82',
+            'dev_requirement' => false,
+        ),
+    ),
+);
